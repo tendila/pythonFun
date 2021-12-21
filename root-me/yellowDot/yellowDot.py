33920ed83d1bb10553454e6092cabafb14878e3f
tendila <tendil.alois@gmail.com>
2021-12-21 19:41:15 +1300

    Delete root-me directory
    rm root-me.org public answer :D

--- root-me/yellowDot/yellowDot.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image
-
-# If an image has been scan from a printer (ex: ch18.png)
-blue = Image.open("ch18.png").split()[2]
-blue.point(lambda x: (256-x)**2).show()
-# yellow dot are visible and date & time can be deduct
-
-######## Also try this
-# https://github.com/dfd-tud/deda
